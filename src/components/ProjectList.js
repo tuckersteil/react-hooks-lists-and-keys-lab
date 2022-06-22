f39b28ccad33ef78fd497a3dc73da9090a758df7
Tucker Steil <tuckersteil@gmail.com>
2022-06-22 14:39:34 -0400

    commit tree 4ca7b9d08e9398791ae4e6fe1a70c20d22e751cc

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,19 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectsArray = projects.map((project) => {
+    return <ProjectItem 
+      key={project.id} 
+      name ={project.name} 
+      about={project.abou} 
+      technologies={project.technologies}/>
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectsArray}</div>
     </div>
   );
 }
